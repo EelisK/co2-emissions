b4b487f1da07a13ff4b2851835a2ba8a252fa618
Eelis Kostiainen <eelis.kostiainen@aalto.fi>
2018-12-08 21:13:11 +0200

    Reconfigure dev server

--- webpack/dev.js
+++ webpack/dev.js
@@ -5,9 +5,5 @@ const common = require("./common");
 module.exports = merge(common, {
     mode: "development",
     devtool: "inline-source-map",
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: false,
-        port: 3000
-    }
+    watch: true
 });
