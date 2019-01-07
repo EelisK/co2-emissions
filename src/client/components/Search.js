e8adf9c7676cb7b6bb6abe0c6680f4e0f0787591
Eelis Kostiainen <eelis.kostiainen@aalto.fi>
2019-01-07 20:28:54 +0200

    Prefetch country names before queries

--- src/client/components/Search.js
+++ src/client/components/Search.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Divider, IconButton, InputBase, Paper } from "@material-ui/core";
+import { Divider, IconButton, InputBase, Paper, List, ListItem } from "@material-ui/core";
+import theme from "../config/theme";
+import titleCase from "../util/titleCase";
 import CloseIcon from "../assets/img/close.svg";
 import MagnifyIcon from "../assets/img/magnify.svg";
 
@@ -9,21 +11,64 @@ class Search extends React.Component {
         super(props);
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
+        this.onKeyDown = this.onKeyDown.bind(this);
         this.clearInput = this.clearInput.bind(this);
-        this.state = { country: "" };
+        this.state = { country: "", countries: [], activeIdx: null };
     }
 
     onChange(evt) {
-        this.setState({ country: evt.target.value });
+        const nextVal = evt.target.value;   // Used internally
+        const trimmedVal = nextVal.trim();  // Used for queries
+        this.setState({ country: nextVal, activeIdx: null });
+        if (trimmedVal !== "")
+            this.props
+                .getCountries(trimmedVal)
+                .then(countries => countries.slice(0, 7))
+                // NOTE: this will cause an error if the component is unmounted
+                // but with the current design it shouldn't do that
+                .then(countries => this.setState({ countries }));
+        else
+            this.setState({ countries: [] });
     }
 
-    onSubmit(evt) {
+    onKeyDown(evt) {
+        const escKeyCode = 27;
+        const arrowUpKeyCode = 38;
+        const arrowDownKeyCode = 40;
+        const prevState = this.state;
+        const countryCount = prevState.countries.length;
+        let nextIdx;
+        if (countryCount === 0)
+            return;
+        switch (evt.keyCode) {
+            case arrowUpKeyCode:
+                nextIdx = (prevState.activeIdx || countryCount) - 1;
+                break;
+            case arrowDownKeyCode:
+                nextIdx = (((prevState.activeIdx === null ? -1 : prevState.activeIdx) + 1) % countryCount);
+                break;
+            case escKeyCode:
+                this.setState({ activeIdx: null });
+            default:
+                return;
+        }
+        this.setState({ activeIdx: nextIdx, country: prevState.countries[nextIdx] });
+    }
+
+    /**
+     * Fetch emissions with from the given country/countries
+     * @param {*} evt The submit event
+     * @param {*} country Country which will be used to fetch the emission data.
+     * Default is this.state.country
+     */
+    onSubmit(evt, country = this.state.country) {
         evt.preventDefault();
-        this.props.fetchEmissions(this.state.country);
+        this.setState({ countries: [], country });
+        this.props.fetchEmissions(country);
     }
 
     clearInput() {
-        this.setState({ country: "" });
+        this.setState({ country: "", countries: [] });
         this.inputRef && this.inputRef.focus();
     }
 
@@ -37,6 +82,7 @@ class Search extends React.Component {
                         value={this.state.country}
                         placeholder="Search by country"
                         className="co2-app-search-input"
+                        onKeyDown={this.onKeyDown}
                         inputRef={ref => this.inputRef = ref} />
                     <IconButton className="co2-app-search-icon-button" type="submit">
                         <img src={MagnifyIcon} />
@@ -49,6 +95,18 @@ class Search extends React.Component {
                     <IconButton className="co2-app-search-icon-button" onClick={this.clearInput}>
                         <img src={CloseIcon} />
                     </IconButton>
+                </Paper>
+                <Paper className="co2-app-countries-list-container" elevation={3}>
+                    <List className="co2-app-countries-list" style={{ background: theme.palette.background.default }}>
+                        {this.state.countries.map((country, idx) => {
+                            return (
+                                <ListItem button key={country} onClick={evt => this.onSubmit(evt, country)} onKeyDown={this.onKeyDown}
+                                    style={{ color: theme.typography.button.color }} selected={this.state.activeIdx === idx}>
+                                    {titleCase(country)}
+                                </ListItem>
+                            );
+                        })}
+                    </List>
                 </Paper>
             </form>
         );
--- src/client/containers/SearchContainer.js
+++ src/client/containers/SearchContainer.js
@@ -1,7 +1,20 @@
+import React from "react";
 import { connect } from "react-redux";
 import Search from "../components/Search";
 import fetchCO2Emissions from "../actions/fetchCO2Emissions";
 import FetchStatus from "../util/FetchStatus";
+import HttpRequest from "../util/HttpRequest";
+
+/**
+ * Handle the country name previews in here
+ * and render them in the representational component Search.
+ * This is completely fine as long as the Search component never unmounts.
+ * If that will ever become necessary this logic should be handled with Redux.
+ * @param {*} name String that is matched with the possible countries
+ */
+const getCountries = name => {
+    return new HttpRequest(`/api/v1/countries/${name}`).send().then(x => x.data);
+};
 
 const mapStateToProps = state => {
     return {
@@ -15,4 +28,4 @@ const mapDispatchToProps = dispatch => {
     }
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Search);
+export default connect(mapStateToProps, mapDispatchToProps)(props => <Search {...props} getCountries={getCountries} />);
