c732a60022d125245ecff3efcd60d78837294d70
Eelis Kostiainen <eelis.kostiainen@aalto.fi>
2019-01-08 23:34:30 +0200

    Optimize bundle

--- webpack/common.js
+++ webpack/common.js
@@ -29,7 +29,7 @@ module.exports = {
                 use: [{
                     loader: "file-loader",
                     options: {
-                        name: "[name].[ext]",
+                        name: "[hash].[ext]",
                         outputPath: "img/"
                     }
                 }]
--- webpack/prod.js
+++ webpack/prod.js
@@ -1,6 +1,25 @@
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 const merge = require("webpack-merge");
 const common = require("./common");
 
 module.exports = merge(common, {
-    mode: "production"
+    mode: "production",
+    optimization: {
+        minimizer: [
+            new UglifyJSPlugin({
+                parallel: true,
+                cache: true,
+                uglifyOptions: {
+                    mangle: true,
+                    keep_fnames: false,
+                    keep_classnames: false,
+                    compress: true
+                },
+                extractComments: true
+            })
+        ]
+    },
+    output: {
+        filename: "bundle.[hash].js"
+    }
 });
